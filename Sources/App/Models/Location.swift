5bbe038baf265e2639e5cc22b1780e559eaa9539
Viktor Nakyden <viktor.nakyden@intive.com>
2019-07-30 13:09:35 +0200

    cherry pick from `feature/decode_once` with new models.

--- Sources/App/Models/Location.swift
+++ Sources/App/Models/Location.swift
@@ -11,23 +11,35 @@ enum LocationID: String, Codable {
     case muc = "MUC"
     case ber = "BER"
     case reg = "REG"
+
+    var offices: [Office] {
+        return Office.mock(for: self)
+    }
+
+    var officesNumber: Int {
+        return offices.count
+    }
 }
 
 struct Location: Codable {
     let id: LocationID
     let city: String
-    let offices: [Office]
+    let officesNumber: Int
     let imageUrl: String
 
     static var mocks: [Location] {
+        let reg: LocationID = .reg
+        let muc: LocationID = .muc
+        let ber: LocationID = .ber
         return [
-            Location(id: .muc, city: "Munich", offices: [], imageUrl: "https://munichnow.com/wp-content/uploads/2017/03/59628-17082366_304.jpg"),
-            Location(id: .ber, city: "Berlin", offices: [], imageUrl: "https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg"),
-            Location(id: .reg, city: "Regensburg", offices: [], imageUrl: "https://www.dw.com/image/16946292_303.jpg")
+            Location(id: muc, city: "Munich", officesNumber: muc.officesNumber, imageUrl: "https://munichnow.com/wp-content/uploads/2017/03/59628-17082366_304.jpg"),
+            Location(id: ber, city: "Berlin", officesNumber: ber.officesNumber, imageUrl: "https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg"),
+            Location(id: reg, city: "Regensburg", officesNumber: reg.officesNumber, imageUrl: "https://www.dw.com/image/16946292_303.jpg")
         ]
     }
 
     static var mock: Location {
-        return  Location(id: .muc, city: "Munich", offices: [], imageUrl: "https://munichnow.com/wp-content/uploads/2017/03/59628-17082366_304.jpg")
+        let muc: LocationID = .muc
+        return  Location(id: muc, city: "Munich", officesNumber: muc.officesNumber, imageUrl: "https://munichnow.com/wp-content/uploads/2017/03/59628-17082366_304.jpg")
     }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -42,7 +42,7 @@ public func routes(_ wss: NIOWebSocketServer) {
                     print("sent: \(messageToClient)")
                 }
             case .office:
-                if let locationId = LocationID(rawValue: messageData.data ?? ""), let messageToClient = Message.prepare(id: messageData.id, event: .office, data: Office.mock(for: locationId))?.asString() {
+                if let locationId = LocationID(rawValue: messageData.data ?? ""), let messageToClient = Message.prepare(id: messageData.id, event: .office, data: locationId.offices)?.asString() {
                     ws.send(messageToClient)
                     print("sent: \(messageToClient)")
                 }
